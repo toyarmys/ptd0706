366238f66a63581d0d83b13f256eb917a4f82133
toyarmys <59601084+toyarmys@users.noreply.github.com>
2020-01-11 02:25:17 +0700

    Add files via upload

--- Darnilla/JS/index.js
+++ Darnilla/JS/index.js
@@ -3,7 +3,6 @@ function openNav() {
   document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
   document.getElementById("openSpan").style.visibility = "hidden";
   document.getElementById("close").style.visibility = "visible";
-
 }
 
 function closeNav() {
@@ -21,8 +20,8 @@ for (var i = 0; i < btns.length; i++) {
   var current = document.getElementsByClassName("current-page");
   current[0].className = current[0].className.replace(" current-page", "");
   this.className += " current-page";
-  });
-}
+    });
+  }
 });
 
 $(document).ready(function () {
@@ -71,3 +70,91 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+
+    $('.scroll-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+});
+
+var page = 1;
+
+function showPage(page) {
+    $('.pagination .post:not(#page'+page+')').hide();
+    $('.pagination .post#page'+page).show();
+}
+
+function prevPage() {
+    if (page == 1) {
+        page = $('.pagination .post').length;
+    } else {
+        page--;
+    }
+    showPage(page);
+}
+
+function nextPage() {
+    if (page == $('.pagination .post').length) {
+        page = 1;
+    } else {
+        page++;
+    }
+    showPage(page);
+}
+
+$(document).ready(function() {
+    showPage(page);
+    $('#prev').click(prevPage);
+    $('#next').click(nextPage);
+});
+
+var a = document.getElementsByClassName("prev-next");
+var b = document.getElementsByClassName("prev-next-btn");
+$(function(){
+for (var i = 0; i < b.length; i++) {
+  b[i].addEventListener("click", function() {
+  var currents = document.getElementsByClassName("activate");
+  currents[0].className = currents[0].className.replace(" activate", "");
+  this.className += " activate";
+    });
+  }
+}); 
+
+let saveFile = () => 
+{
+  const name = document.getElementById('Name');
+  const email = document.getElementById('Mail');
+  const title = document.getElementById('Title');
+  const comment = document.getElementById('Comment');
+  let data = 
+      '\r Name: ' + name.value + ' \r\n ' + 
+      'Email: ' + email.value + ' \r\n ' + 
+      'Country: ' + title.value + ' \r\n ' + 
+      'Message: ' + comment.value;
+  const textToBLOB = new Blob([data], { type: 'text/plain' });
+  const sFileName = 'formData.txt';    // The file to save the data.
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+  if (window.webkitURL != null) {
+      newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  }
+  else {
+      newLink.href = window.URL.createObjectURL(textToBLOB);
+      newLink.style.display = "none";
+      document.body.appendChild(newLink);
+  }
+  newLink.click(); 
+}
